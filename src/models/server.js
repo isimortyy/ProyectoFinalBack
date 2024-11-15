46cc70a96ce9a283c3f55ff2b356dd62d697d2e7
yonathanblanco <yonathan_lopez@soy.sena.edu.co>
2024-11-14 22:19:18 -0500

    Eliminar archivo index.html y optimizar importaciones en server.js

--- src/models/server.js
+++ src/models/server.js
@@ -3,58 +3,51 @@ import http from 'http';
 import cors from 'cors';
 import { dbconnect } from "../../databases/config.js"
 
-import apprenticeR from '../routes/apprentice.js'
-// import assignamentR from '../routes/assignment.js'
-import binnaclesR from '../routes/binnacles.js'
-import followupR from '../routes/followup.js'
-import logsR from '../routes/log.js'
-import modalityR from '../routes/modality.js'
+import  apprenticeR   from '../routes/apprentice.js'
+/* import  assignamentR  from '../routes/assignment.js' */
+import  binnaclesR  from '../routes/binnacles.js'
+import  followupR  from '../routes/followup.js'
+import  logsR  from '../routes/log.js'
+import  modalityR  from '../routes/modality.js'
 import register from '../routes/register.js';
-import repfora from '../routes/repfora.js';
-import path from 'path';  // Necesitamos esta importación para resolver rutas de archivos estáticos
+import  repfora from '../routes/repfora.js'
+
 
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT 
         this.server = http.createServer(this.app);
-
+    
         // Middlewares
         this.middlewares();
 
         // Rutas de mi aplicación
         this.routes();
 
-        // Conexión a la base de datos
-        this.conectarbd();
+        this.conectarbd()
     }
 
-    async conectarbd() {
+    async conectarbd(){
         await dbconnect();
     }
 
     middlewares() {
         this.app.use(cors());
         this.app.use(express.json());
-
-        // Servir archivos estáticos desde la carpeta 'public'
-        this.app.use(express.static(path.join(__dirname, 'public'))); // Sirve la carpeta 'public'
+        this.app.use(express.static('public'));
     }
 
     routes() {
         this.app.use('/api/apprentice', apprenticeR);
-        // this.app.use('/api/assignment', assignamentR);
+       /*  this.app.use('/api/assignment', assignamentR); */
         this.app.use('/api/binnacles', binnaclesR);
         this.app.use('/api/followup', followupR);
         this.app.use('/api/log', logsR);
         this.app.use('/api/modality', modalityR);
-        this.app.use('/api/register', register);
-        this.app.use('/api/repfora', repfora);
-
-        // Redirigir todas las rutas no encontradas al 'index.html' de Vue
-        this.app.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, 'public', 'index.html')); // Redirige a index.html
-        });
+        this.app.use('/api/register',register)
+        this.app.use('/api/repfora',repfora)
+        
     }
 
     listen() {
@@ -63,5 +56,4 @@ class Server {
         });
     }
 }
-
 export { Server };
