4ecb1e324dca3f122abe5790cc3ed2cf1b0ae85f
yonathanblanco <yonathan_lopez@soy.sena.edu.co>
2024-11-14 21:25:17 -0500

    Refactor server.js: mejorar la organización de importaciones y añadir manejo de rutas no encontradas

--- src/models/server.js
+++ src/models/server.js
@@ -3,51 +3,58 @@ import http from 'http';
 import cors from 'cors';
 import { dbconnect } from "../../databases/config.js"
 
-import  apprenticeR   from '../routes/apprentice.js'
-/* import  assignamentR  from '../routes/assignment.js' */
-import  binnaclesR  from '../routes/binnacles.js'
-import  followupR  from '../routes/followup.js'
-import  logsR  from '../routes/log.js'
-import  modalityR  from '../routes/modality.js'
+import apprenticeR from '../routes/apprentice.js'
+// import assignamentR from '../routes/assignment.js'
+import binnaclesR from '../routes/binnacles.js'
+import followupR from '../routes/followup.js'
+import logsR from '../routes/log.js'
+import modalityR from '../routes/modality.js'
 import register from '../routes/register.js';
-import  repfora from '../routes/repfora.js'
-
+import repfora from '../routes/repfora.js';
+import path from 'path';  // Necesitamos esta importación para resolver rutas de archivos estáticos
 
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.PORT 
+        this.port = process.env.PORT;
         this.server = http.createServer(this.app);
-    
+
         // Middlewares
         this.middlewares();
 
         // Rutas de mi aplicación
         this.routes();
 
-        this.conectarbd()
+        // Conexión a la base de datos
+        this.conectarbd();
     }
 
-    async conectarbd(){
+    async conectarbd() {
         await dbconnect();
     }
 
     middlewares() {
         this.app.use(cors());
         this.app.use(express.json());
-        this.app.use(express.static('public'));
+
+        // Servir archivos estáticos desde la carpeta 'public'
+        this.app.use(express.static(path.join(__dirname, 'public'))); // Sirve la carpeta 'public'
     }
 
     routes() {
         this.app.use('/api/apprentice', apprenticeR);
-       /*  this.app.use('/api/assignment', assignamentR); */
+        // this.app.use('/api/assignment', assignamentR);
         this.app.use('/api/binnacles', binnaclesR);
         this.app.use('/api/followup', followupR);
         this.app.use('/api/log', logsR);
         this.app.use('/api/modality', modalityR);
-        this.app.use('/api/register',register)
-        this.app.use('/api/repfora',repfora)
-        
+        this.app.use('/api/register', register);
+        this.app.use('/api/repfora', repfora);
+
+        // Redirigir todas las rutas no encontradas al 'index.html' de Vue
+        this.app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'public', 'index.html')); // Redirige a index.html
+        });
     }
 
     listen() {
@@ -56,4 +63,5 @@ class Server {
         });
     }
 }
+
 export { Server };
